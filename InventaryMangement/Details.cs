f7bbac1faa629b6c4935aae25c2fc067803cf2e7
unknown <satya-1298>
2023-04-04 14:05:25 +0530

    Inventary Details

---
+++ InventaryMangement/Details.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryMangement
+{
+    public class Details
+    {
+
+        public List<TypesOfRices> typesOfRices;
+        public List<TypesOfWheats> typesOfWheats;
+        public List<TypesOfPulses> typesOfPulses;
+    }
+    public class TypesOfRices
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheats
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
