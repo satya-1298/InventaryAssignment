79ec515699237a43c2675b4d717a9728dad06a88
unknown <satya-1298>
2023-04-04 16:59:16 +0530

    Read Inventary

---
+++ InventaryMangement/Read.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace InventaryMangement
+{
+    public class Reads
+    {
+        public Details Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Details>(json);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
