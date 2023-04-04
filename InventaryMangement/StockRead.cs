bf10b0e1cf2158caa04c2856199ef0a24a22cbed
unknown <satya-1298>
2023-04-04 18:03:36 +0530

    Read path

---
+++ InventaryMangement/StockRead.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryMangement
+{
+    public class StockRead
+    {
+        public StockDetails Reads(string path2)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path2))
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockDetails>(json);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
