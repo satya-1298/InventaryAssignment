41794529ac67c273861789e2d880d137ffa7f949
unknown <satya-1298>
2023-04-04 18:03:06 +0530

    Details

---
+++ InventaryMangement/StockDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaryMangement
+{
+    public  class StockDetails
+    {
+        public List<StockReport> stockReport;
+        
+    }
+    public class StockReport
+    {
+        public string name;
+        public int Stocks;
+        public int price;
+    }
+}
