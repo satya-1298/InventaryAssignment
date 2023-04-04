b46755e27bb5e2f3ebd5fb9f2380bace1dfe38e2
unknown <satya-1298>
2023-04-04 14:02:13 +0530

    Greeting

---
+++ InventaryMangement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventaryMangement
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("These are object oriented program");
+        }
+    }
+}
