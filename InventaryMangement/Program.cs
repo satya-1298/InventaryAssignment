0e36bf265f45315b8c3f7f97244fe6affa96e693
unknown <satya-1298>
2023-04-04 16:57:33 +0530

    calculate Rates

--- InventaryMangement/Program.cs
+++ InventaryMangement/Program.cs
@@ -5,6 +5,48 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("These are object oriented program");
+            string path = "D:\\assignments\\InventaryRepo\\InventaryAssignment\\InventaryMangement\\Data.json";
+            Reads obj = new Reads();
+            Details data = obj.Read(path);
+
+
+            Console.WriteLine("Types of Rice are:");
+            Console.WriteLine("*********************");
+            for (int i = 0; i < data.typesOfRices.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRices[i].name);
+                Console.WriteLine(data.typesOfRices[i].weight);
+                Console.WriteLine(data.typesOfRices[i].price);
+                int val = data.typesOfRices[i].price * data.typesOfRices[i].weight;
+                Console.WriteLine("The price of {0} is {1} ",data.typesOfRices[i].name,val); 
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Types of Wheats Are");
+            Console.WriteLine("**********");
+            for (int i = 0; i < data.typesOfWheats.Count; i++)
+            {
+                Console.WriteLine(data.typesOfWheats[i].name);
+                Console.WriteLine(data.typesOfWheats[i].weight);
+                Console.WriteLine(data.typesOfWheats[i].price);
+                int val = data.typesOfWheats[i].price * data.typesOfWheats[i].weight;
+                Console.WriteLine("The price of {0} is {1} ", data.typesOfWheats[i].name, val);
+
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Types of pulses Are");
+            Console.WriteLine("*************");
+            for (int i = 0; i < data.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typesOfPulses[i].name);
+                Console.WriteLine(data.typesOfPulses[i].weight);
+                Console.WriteLine(data.typesOfPulses[i].price);
+                int val = data.typesOfPulses[i].price * data.typesOfPulses[i].weight;
+                Console.WriteLine("The price of {0} is {1} ", data.typesOfPulses[i].name, val);
+
+
+            }
         }
     }
 }
